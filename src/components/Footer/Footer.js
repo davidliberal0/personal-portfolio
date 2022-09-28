38f3dc957ef0104e30c273868b6edf44253539a9
davidliberal0 <davidliberal0@gmail.com>
2022-09-28 11:47:44 -0400

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+    CompanyContainer,
+    FooterWrapper,
+    LinkColumn,
+    LinkItem,
+    LinkList,
+    LinkTitle,
+    Slogan,
+    SocialContainer,
+    SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
+    return (
+        <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle>Call</LinkTitle>
+                    <LinkItem href="tel: 407-797-7547">407-797-7547</LinkItem>
+                </LinkColumn>
+                <LinkColumn>
+                    <LinkTitle>Email</LinkTitle>
+                    <LinkItem href="mailto:dliberal1022@gmail.com">
+                        dliberal1022@gmail.com
+                    </LinkItem>
+                </LinkColumn>
+            </LinkList>
+            <SocialIconsContainer>
+                <CompanyContainer>
+                    <Slogan>sdfgdsfdds</Slogan>
+                </CompanyContainer>
+                <SocialContainer>
+                    <SocialIcons href="https://github.com/davidliberal0">
+                        <AiFillGithub size="3rem" />
+                    </SocialIcons>
+                    <SocialIcons href="https://www.linkedin.com/in/david-liberal-2380b21b3/">
+                        <AiFillLinkedin size="3rem" />
+                    </SocialIcons>
+                    <SocialIcons href="https://www.instagram.com/nightly.dev/">
+                        <AiFillInstagram size="3rem" />
+                    </SocialIcons>
+                </SocialContainer>
+            </SocialIconsContainer>
+        </FooterWrapper>
+    );
 };
 
 export default Footer;
