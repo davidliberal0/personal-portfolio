a05d049c3be6a1660eea439466df17a07836ba9e
davidliberal0 <davidliberal0@gmail.com>
2022-09-29 17:08:55 -0400

    minor update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,11 +10,11 @@ export const projects = [
         id: 0,
     },
     {
-        title: "E-Commerce",
+        title: "Travel Landing Page",
         description:
             "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
         image: "/images/2.png",
-        tags: ["React", "JavaScript"],
+        tags: ["HTML", "CSS"],
         source: "https://google.com",
         visit: "https://google.com",
         id: 1,
